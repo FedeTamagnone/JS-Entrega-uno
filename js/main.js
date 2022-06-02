770db3582556ed964b18c1e94c01b6e651d5b520
FedeTamagnone <103047702+FedeTamagnone@users.noreply.github.com>
2022-06-02 13:03:25 -0300

    Creando git

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let temperatura = parseInt(prompt("Ingresa la temperatura actual"));
+if (temperatura <= 10) {
+    alert("¡Hace mucho frio! LLeva campera")
+} else if (temperatura <= 18) {
+    alert("Está frezco, con un abrigo ligero vas a estar bien ")
+} else {
+    alert("Está calido, que tengas un lindo día")
+}
+
