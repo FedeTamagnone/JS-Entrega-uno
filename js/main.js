12f4757369c0ba663d057c673699d516631de027
FedeTamagnone <103047702+FedeTamagnone@users.noreply.github.com>
2022-06-02 13:23:35 -0300

    Tarea JS

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
+alert ("Hola, ¿Estás por salir de casa? No olvides de chequear el tiempo!")
+
 let temperatura = parseInt(prompt("Ingresa la temperatura actual"));
 if (temperatura <= 10) {
     alert("¡Hace mucho frio! LLeva campera")
 } else if (temperatura <= 18) {
     alert("Está frezco, con un abrigo ligero vas a estar bien ")
 } else {
-    alert("Está calido, que tengas un lindo día")
+    alert("Está calido, hermoso día !")
 }
 
+let productos = prompt("Ingresa los articulos que debes comprar");
+while (productos != "salir"){
+    console.log(productos);
+    productos = prompt("Ingresa otro producto de lo contrario escribí salir");
+}
